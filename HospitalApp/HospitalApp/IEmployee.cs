6472129cb31f83dff2814bb945d4c48a30371417
Ivan Andrienko <wdprogr@gmail.com>
2016-07-12 11:18:14 +0300

    Add IEmployee, add class Doctor

---
+++ HospitalApp/HospitalApp/IEmployee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalApp
+{
+    interface IEmployee
+    {
+        string FirstName { get; set; }
+        string SecondName { get; set; }
+        string Position { get; set; }
+        string StartingOfficeHours { get; set; }
+        string EndOfficeHours { get; set; }
+        string Cellphone { get; set; }
+    }
+}
