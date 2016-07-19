92df9d2d8489c29bbcc7960e949ef6f0470f0dfc
Ivan Andrienko <wdprogr@gmail.com>
2016-07-19 19:36:09 +0300

    classies 4

---
+++ HospitalApp/HospitalApp/Context/ContactInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalApp.Context
+{
+    class ContactInfo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string IdUser { get; set; }
+        public string Type { get; set; }
+        public string Contact { get; set; }
+        public bool Default { get; set; }
+    }
+}
---
+++ HospitalApp/HospitalApp/Context/Record.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalApp.Context
+{
+    class Record
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int IdDoctor { get; set; }
+        public int IdPatient { get; set; }
+        public int Disease { get; set; }
+        public int Medicine { get; set; }
+        public DateTime RecordDate { get; set; }
+        public DateTime DischargeDate { get; set; }
+        public string AdditionalInfo { get; set; }
+    }
+}
