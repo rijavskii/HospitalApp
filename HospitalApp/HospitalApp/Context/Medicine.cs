01197100bd4f75573f74dffe5ad9cf8d6f322b7d
Alexandr Kalugin <kalugin.alexandr.vn@gmail.com>
2016-07-20 15:56:44 +0300

    Add Medicine,MedicineType,Disease

---
+++ HospitalApp/HospitalApp/Context/Medicine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalApp.Context
+{
+    public class Medicine
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        [Index("IndexMedicine", 1, IsUnique = true)]
+        public String Name { get; set; }
+
+        [Index("IndexMedicine",2,IsUnique = true)]
+        public Manufacturer Manufacturer { get; set; }
+
+        [Index("IndexMedicine", 3, IsUnique = true)]
+        public MedicineType MedicineType { get;set; }
+    }
+}
